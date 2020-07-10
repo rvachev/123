f168ddb5e30abb1070958366ae9d95441e3d7b11
Roman <romandauria@gmail.com>
2020-07-10 16:47:58 +0600

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double t = 0.392;
+            double x = Math.Sin(t) * 1.8;
+            double y = (t + 4) / (Math.Sin(t) * x);
+            double result = Math.Pow(x, 3) * Math.Exp(-1 * x * y) * (Math.Sqrt(0.8 * x * y * t) / (Math.Cos(x) * Math.Log(y)));
+            Console.WriteLine("Результат вычисления - " + result);
+            Console.ReadKey();
+        }
+    }
+}
